a9c4463fe8d16cfb485c1c684cdc2133d3202842
monayemhossain90 <monayemhossain1990@gmail.com>
2025-08-09 22:15:49 +0600

    1.1 practice

---
+++ 1.1/script.js
@@ -0,0 +1,23 @@
+// select dom elements
+
+const counterEl = document.getElementById("counter")
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+
+// initial state
+
+let count = 0;
+
+//  add event listener
+
+incrementEl.addEventListener("click",()=>{
+    count++;
+    counterEl.innerText = count;
+});
+
+decrementEl.addEventListener("click",()=>{
+    count--;
+
+    counterEl.innerText = count;
+})
